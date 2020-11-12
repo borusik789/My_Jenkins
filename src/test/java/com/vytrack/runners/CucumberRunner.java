6946db48cacc69fdbaf00fbe402100a269e3fa6a
borusik789 <borusik789@gmail.com>
2020-11-12 14:38:22 +0300

    Changed ip

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@smoke and @me",
         publish = true
 )
 public class CucumberRunner {
